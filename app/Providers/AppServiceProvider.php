494eaccb201fa358b3ee9940a72b0293efec74ed
lzaragosa555459-eng <l.zaragosa.555459@umindanao.edu.ph>
2026-04-29 16:48:46 +0800

    added dockerfile and .dockerignore

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,25 +1,55 @@
 <?php
 
-namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+
+namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 
+
+use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Support\Facades\URL;
+
+
+
 class AppServiceProvider extends ServiceProvider
+
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
 
     /**
-     * Bootstrap any application services.
+
+     * Register any application services.
+
      */
+
+    public function register(): void
+
+    {
+
+        //
+
+    }
+
+
+
+    /**
+
+     * Bootstrap any application services.
+
+     */
+
     public function boot(): void
+
     {
         Paginator::useBootstrap();
+        
+        if (env('APP_ENV') === 'production') {
+
+            URL::forceScheme('https');
+
+        }
+
     }
+
 }
+
